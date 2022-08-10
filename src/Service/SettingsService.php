5b12ba41f0d5183f955c728b52d3d599fe5957bc
Nemanja <nemanja14zivanovic@gmail.com>
2022-08-10 16:31:17 +0200

    Add settings service

---
+++ src/Service/SettingsService.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Coincharge\ShopwareBTCPay\Service;
+
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+
+class SettingsService
+{
+    /**
+     * @var SystemConfigService
+     */
+    private $systemConfigService;
+
+    /**
+     * @param SystemConfigService $systemConfigService
+     */
+    public function __construct(SystemConfigService $systemConfigService)
+    {
+        $this->systemConfigService = $systemConfigService;
+    }
+
+    public function getSetting(string $setting, ?string $salesChannelId=null) : mixed
+    {
+        return $this->systemConfigService->get("ShopwareBTCPay.config. ".$setting,$salesChannelId);
+    }
+    public function getShopName(string $salesChannelId) : string
+    {
+        return $this->systemConfigService->get("core.basicInformation.shopName");
+    }
+
+}
