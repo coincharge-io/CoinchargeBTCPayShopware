873a0d5de4a74934249bfc4bd99cb87572666b34
Nemanja <nemanja14zivanovic@gmail.com>
2022-09-11 10:13:50 +0200

    Rename file

--- src/Service/BTCPayPayment.php
+++ src/Service/BTCPayPayment.php
@@ -17,7 +17,7 @@ use GuzzleHttp\Client;
 use Psr\Log\LoggerInterface;
 use Coincharge\Shopware\Service\ConfigurationService;
 
-class CoinchargeBTCPayPayment implements AsynchronousPaymentHandlerInterface
+class BTCPayPayment implements AsynchronousPaymentHandlerInterface
 {
     private ConfigurationService  $configurationService;
     private LoggerInterface $logger;
