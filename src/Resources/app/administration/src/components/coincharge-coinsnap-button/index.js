2c11af727dfa2970e277e123fe2491a577713e73
www-data <www-data@localhost>
2023-08-23 15:26:25 +0200

    Fix test connection

--- src/Resources/app/administration/src/components/coincharge-coinsnap-button/index.js
+++ src/Resources/app/administration/src/components/coincharge-coinsnap-button/index.js
@@ -17,27 +17,34 @@ Component.register("coincharge-coinsnap-button", {
 	data() {
 		return {
 			isLoading: false,
-			config: {
-				"CoinchargeBTCPayShopware.config.coinsnapStoreId": "",
-				"CoinchargeBTCPayShopware.config.coinsnapApiKey": "",
-			},
+			coinsnapStoreId: "",
+			coinsnapApiKey: "",
 		};
+	},
+	mounted() {
+		const systemConfig = ApiService.getByName("systemConfigApiService");
+		systemConfig
+			.getValues("CoinchargeBTCPayShopware.config")
+			.then((r) => {
+				this.coinsnapApiKey =
+					r["CoinchargeBTCPayShopware.config.coinsnapApiKey"];
+				this.coinsnapStoreId =
+					r["CoinchargeBTCPayShopware.config.coinsnapStoreId"];
+			})
+			.catch((e) => console.log(e));
+	},
+	computed: {
+		isDisabled() {
+			if (!this.coinsnapStoreId || !this.coinsnapApiKey) {
+				return true;
+			}
+		},
 	},
 	methods: {
 		testConnection() {
 			this.isLoading = true;
-			const systemConfig = ApiService.getByName("systemConfigApiService");
-			const coinsnapStoreId = document.getElementById(
-				"CoinchargeBTCPayShopware.config.coinsnapStoreId"
-			).value;
-			const coinsnapApiKey = document.getElementById(
-				"CoinchargeBTCPayShopware.config.coinsnapApiKey"
-			).value;
-			systemConfig.saveValues({
-				"CoinchargeBTCPayShopware.config.coinsnapApiKey": coinsnapApiKey,
-				"CoinchargeBTCPayShopware.config.coinsnapStoreId": coinsnapStoreId,
-			});
-			if (coinsnapApiKey == "" || coinsnapStoreId == "") {
+			if (!this.credentialsExist()) {
+				this.isLoading = false;
 				return this.createNotificationWarning({
 					title: "BTCPay Server",
 					message: this.$tc(
@@ -71,6 +78,37 @@ Component.register("coincharge-coinsnap-button", {
 						message: this.$tc("coincharge-coinsnap-test-connection.error"),
 					});
 				});
+		},
+		saveCredentials() {
+			const systemConfig = ApiService.getByName("systemConfigApiService");
+			const coinsnapStoreId = document.getElementById(
+				"CoinchargeBTCPayShopware.config.coinsnapStoreId"
+			).value;
+			const coinsnapApiKey = document.getElementById(
+				"CoinchargeBTCPayShopware.config.coinsnapApiKey"
+			).value;
+			systemConfig.saveValues({
+				"CoinchargeBTCPayShopware.config.coinsnapStoreId": coinsnapStoreId,
+				"CoinchargeBTCPayShopware.config.coinsnapApiKey": coinsnapApiKey,
+			});
+			window.location.reload();
+		},
+		credentialsExist() {
+			const systemConfig = ApiService.getByName("systemConfigApiService");
+			if (
+				document.getElementById(
+					"CoinchargeBTCPayShopware.config.coinsnapStoreId"
+				).value == "" ||
+				document.getElementById(
+					"CoinchargeBTCPayShopware.config.coinsnapApiKey"
+				).value == ""
+			) {
+				systemConfig.saveValues({
+					"CoinchargeBTCPayShopware.config.coinsnapIntegrationStatus": false,
+				});
+				return false;
+			}
+			return true;
 		},
 	},
 });
