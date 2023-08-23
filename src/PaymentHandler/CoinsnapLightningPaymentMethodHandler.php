acaaa75f80cbfc345446b1e2cf25e0ea13cc1879
www-data <www-data@localhost>
2023-08-23 16:21:01 +0200

    Fix redirect url

--- src/PaymentHandler/CoinsnapLightningPaymentMethodHandler.php
+++ src/PaymentHandler/CoinsnapLightningPaymentMethodHandler.php
@@ -21,49 +21,49 @@ use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStat
 
 class CoinsnapLightningPaymentMethodHandler extends AbstractPaymentMethodHandler
 {
-  private ClientInterface $client;
-  private ConfigurationService  $configurationService;
-  private OrderTransactionStateHandler $transactionStateHandler;
-  private LoggerInterface $logger;
+    private ClientInterface $client;
+    private ConfigurationService  $configurationService;
+    private OrderTransactionStateHandler $transactionStateHandler;
+    private LoggerInterface $logger;
 
-  public function __construct(ClientInterface $client, ConfigurationService $configurationService, OrderTransactionStateHandler $transactionStateHandler, LoggerInterface $logger)
-  {
-    $this->client = $client;
-    $this->configurationService = $configurationService;
-    $this->transactionStateHandler = $transactionStateHandler;
-    $this->logger = $logger;
-    parent::__construct($client, $configurationService, $transactionStateHandler, $logger);
-  }
-  public function sendReturnUrlToCheckout(AsyncPaymentTransactionStruct $transaction, SalesChannelContext $context)
-  {
-    try {
-      $accountUrl = $this->baseSuccessUrl . $transaction->getOrderTransaction()->getOrderId();
-      if ($transaction->getOrderTransaction()->getAmount()->getTotalPrice() == 0) {
-        $this->transactionStateHandler->paid($transaction->getOrderTransaction()->getId(), $context->getContext());
-        return $accountUrl;
-      }
-      $uri = '/api/v1/stores/' . $this->configurationService->getSetting('coinsnapStoreId') . '/invoices';
-      $response = $this->client->sendPostRequest(
-        $uri,
-        [
-          'amount' => $transaction->getOrderTransaction()->getAmount()->getTotalPrice(),
-          'currency' => $context->getCurrency()->getIsoCode(),
-          'referralCode'=>'DEV17612c35cd8c54d3fad381615',
-          'metadata' =>
-          [
-            'orderNumber' => $transaction->getOrder()->getOrderNumber(),
-            'transactionId' => $transaction->getOrderTransaction()->getId()
-          ],
-          'orderId' => $transaction->getOrderTransaction()->getOrderId(),
-          'redirectURL' => $accountUrl,
-          'paymentMethods' => ['Lightning']
-        ]
-      );
-
-      return $response['checkoutLink'];
-    } catch (\Exception $e) {
-      $this->logger->error($e->getMessage());
-      throw new \Exception($e->getMessage());
+    public function __construct(ClientInterface $client, ConfigurationService $configurationService, OrderTransactionStateHandler $transactionStateHandler, LoggerInterface $logger)
+    {
+        $this->client = $client;
+        $this->configurationService = $configurationService;
+        $this->transactionStateHandler = $transactionStateHandler;
+        $this->logger = $logger;
+        parent::__construct($client, $configurationService, $transactionStateHandler, $logger);
     }
-  }
+    public function sendReturnUrlToCheckout(AsyncPaymentTransactionStruct $transaction, SalesChannelContext $context)
+    {
+        try {
+            $accountUrl = $this->baseSuccessUrl . $transaction->getOrderTransaction()->getOrderId();
+            if ($transaction->getOrderTransaction()->getAmount()->getTotalPrice() == 0) {
+                $this->transactionStateHandler->paid($transaction->getOrderTransaction()->getId(), $context->getContext());
+                return $accountUrl;
+            }
+            $uri = '/api/v1/stores/' . $this->configurationService->getSetting('coinsnapStoreId') . '/invoices';
+            $response = $this->client->sendPostRequest(
+                $uri,
+                [
+                'amount' => $transaction->getOrderTransaction()->getAmount()->getTotalPrice(),
+                'currency' => $context->getCurrency()->getIsoCode(),
+                'referralCode' => 'DEV17612c35cd8c54d3fad381615',
+                'metadata' =>
+                [
+                  'orderNumber' => $transaction->getOrder()->getOrderNumber(),
+                  'transactionId' => $transaction->getOrderTransaction()->getId()
+                ],
+                'orderId' => $transaction->getOrderTransaction()->getOrderId(),
+                'redirectUrl' => $accountUrl,
+                'paymentMethods' => ['Lightning']
+        ]
+            );
+
+            return $response['checkoutLink'];
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            throw new \Exception($e->getMessage());
+        }
+    }
 }
