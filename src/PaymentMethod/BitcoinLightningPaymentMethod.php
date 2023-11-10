30168adc0f3953330a3bd4dceeda80c0150256bf
Nemanja <nemanja-z@proton.me>
2023-11-10 12:03:24 +0100

    Fix type for payment method

--- src/PaymentMethod/BitcoinLightningPaymentMethod.php
+++ src/PaymentMethod/BitcoinLightningPaymentMethod.php
@@ -14,39 +14,39 @@ namespace Coincharge\Shopware\PaymentMethod;
 
 use Coincharge\Shopware\PaymentHandler\BitcoinLightningPaymentMethodHandler;
 
-class BitcoinLithningPaymentMethod
+class BitcoinLightningPaymentMethod
 {
-  public function getName(): string
-  {
-    return 'Bitcoin/Lightning';
-  }
+    public function getName(): string
+    {
+        return 'Bitcoin/Lightning';
+    }
 
-  public function getPosition(): int
-  {
-    return -3;
-  }
+    public function getPosition(): int
+    {
+        return -3;
+    }
 
-  public function getTranslations(): array
-  {
-    return [
-      'de-DE' => [
-        'description' => 'Zahle mit Bitcoin/Lightning',
-        'name' => 'Bitcoin/Lightning',
-      ],
-      'en-GB' => [
-        'description' => 'Pay with Bitcoin/Lightning',
-        'name' => 'Bitcoin/Lightning',
-      ],
-      '2fbb5fe2e29a4d70aa5854ce7ce3e20b' => [
-        'description' => 'Pay with Bitcoin/Lightning',
-        'name' => 'Bitcoin/Lightning',
-      ], //Fallback language
-    ];
-  }
+    public function getTranslations(): array
+    {
+        return [
+          'de-DE' => [
+            'description' => 'Zahle mit Bitcoin/Lightning',
+            'name' => 'Bitcoin/Lightning',
+          ],
+          'en-GB' => [
+            'description' => 'Pay with Bitcoin/Lightning',
+            'name' => 'Bitcoin/Lightning',
+          ],
+          '2fbb5fe2e29a4d70aa5854ce7ce3e20b' => [
+            'description' => 'Pay with Bitcoin/Lightning',
+            'name' => 'Bitcoin/Lightning',
+          ], //Fallback language
+        ];
+    }
 
 
-  public function getPaymentHandler(): string
-  {
-    return BitcoinPaymentMethodHandler::class;
-  }
+    public function getPaymentHandler(): string
+    {
+        return BitcoinPaymentMethodHandler::class;
+    }
 }
